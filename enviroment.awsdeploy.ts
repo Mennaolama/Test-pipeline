29cd3229bea13f439b165b483d03228ca83203a5
mennaolama <menna.fathi.olama@vultara.com>
2024-08-22 02:18:38 +0300

    pipiline

---
+++ enviroment.awsdeploy.ts
@@ -0,0 +1,25 @@
+import packageInfo from '../../package.json';
+
+export const environment = {
+    production: true,
+    frontendUrl: "",
+    clientIdSso: "", // active directory registration ID for single sign on,
+    tenantId: "",
+    backendApiUrl: "$APPServerRootHTTPURL",
+    authenticationApiUrl: "$authenticationApiUrl/",
+    exportJira: "",
+    protectModuleLicense: "",
+    vulnerabilityModuleLicense: "",
+    deploymentStatus: "prod",
+    version: packageInfo.version,
+    industry: "",
+    complianceModules:"",
+    loginImageDir: "loginImageDir",
+    socViewUrl: "$socUrl/",
+    soc: "",
+    threatIntelligenceModule: "",
+    taraAutomationModule: "",
+    deploymentType: "",
+    ssoType: "",
+    hideNonSsoLogin: false, // hide/show the non SSO section in login page.
+};
